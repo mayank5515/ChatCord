57ae2bddaa4531a62b19486f55a7ff4c80bc55de
Mayank Chaturvedi <chaturvedimayank70@gmail.com>
2024-06-01 01:53:05 +0530

    IMPLEMENT GETTING USERNAME,TIME,TEXT FROM message in outputMessage

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,8 @@ const chatMessages = document.querySelector(".chat-messages");
 function outputMessage(message) {
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
-    <p class="text">${message}</p>`;
+  div.innerHTML = `<p class="meta">${message.username} <span> ${message.time}</span></p>
+    <p class="text">${message.text}</p>`;
   document.querySelector(".chat-messages").appendChild(div);
 }
 
